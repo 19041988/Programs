b9f53fbb1fa04d69acd8bbe969c54fb51b6707d5
ADMIN <ADMIN@DESKTOP-Q6PLLC7.gateway>
2023-10-09 16:08:10 +1100

    FirstProgrm

---
+++ Maven_Projrct/src/main/java/NewProject/FirstProgram.java
@@ -0,0 +1,12 @@
+package NewProject;
+
+
+public class FirstProgram {
+int x=10,y=20,c;
+public void addNum() {
+	c=x+y;
+
+System.out.println(c);
+}
+
+}
---
+++ Maven_Projrct/src/main/java/NewProject/SecProject.java
@@ -0,0 +1,19 @@
+package NewProject;
+
+
+public class SecProject extends FirstProgram{
+	public void display()
+	{
+	System.out.println("hello");	
+	}
+
+	public static void main(String[] args) {
+	
+		SecProject ob=new SecProject();
+		ob.display();
+		ob.addNum();
+		
+
+	}
+
+}
